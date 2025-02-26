fa4393266512ce3b926ed005cb33bb0fea4aefdb
takokun <mukoudakota8888@gmail.com>
2025-02-26 17:36:06 +0900

    Update index.ts
    環境定数を定義

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-console.log('Hello, world!');
+import { homedir } from 'os';
+import path from 'path';
+
+const syncTargetDir = path.join(homedir(), 'AppData/Local/Packages/Microsoft.MinecraftUWP_8wekyb3d8bbwe/LocalState/games/com.mojang');
+const srcDir = path.resolve('src');
+const buildDir = path.resolve('build');
+const distDir = path.resolve('dist');
+
+const akhsyncFlag = 'akhsync';
+
+export { syncTargetDir, srcDir, buildDir, distDir, akhsyncFlag };
