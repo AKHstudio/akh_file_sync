7632a1b0de6b0d774bfaeab73da17f34d3778ea1
takokun <mukoudakota8888@gmail.com>
2025-02-26 11:05:58 +0900

    Update esbuild.js
    ビルド前にtscで型テストを実行するように変更

--- esbuild.js
+++ esbuild.js
@@ -1,15 +1,31 @@
+import { exec } from 'child_process';
+import { error } from 'console';
 import esbuild from 'esbuild';
 
-esbuild
-    .build({
-        entryPoints: ['src/*.ts'],
-        bundle: true,
-        outdir: 'dist',
-        minify: false,
-        platform: 'node',
-        target: 'ESNext',
-        tsconfig: './tsconfig.json',
-        format: 'esm',
-        packages: 'external',
-    })
-    .catch(() => process.exit(1));
+const tsc = exec('tsc -p ./tsconfig.json --noEmit', { cwd: process.cwd() });
+tsc.stdout.pipe(process.stdout);
+tsc.stderr.pipe(process.stderr);
+
+tsc.on('close', (code) => {
+    if (code === 0) {
+        esbuild
+            .build({
+                entryPoints: ['src/*.ts'],
+                bundle: true,
+                outdir: 'dist',
+                minify: false,
+                platform: 'node',
+                target: 'ESNext',
+                tsconfig: './tsconfig.json',
+                format: 'esm',
+                packages: 'external',
+            })
+            .catch(() => {
+                error('Error building project');
+                process.exit(1);
+            });
+    } else {
+        error('TypeScript compilation failed');
+        process.exit(1);
+    }
+});
