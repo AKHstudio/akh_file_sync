97d5e30d20c4cd80dd26b2cf4ace4d58b6fb5693
takokun <mukoudakota8888@gmail.com>
2025-03-03 22:00:58 +0900

    Update create-pull-requests/package.json
    ビルド時に圧縮しないよいうに変更

--- .github/actions/create-pull-requests/dist/index.js
+++ .github/actions/create-pull-requests/dist/index.js
@@ -1,1 +1,25 @@
-import{context as t,getOctokit as i}from"@actions/github";var{GITHUB_TOKEN:r,TAG_NAME:e}=process.env;async function p(){if(e===void 0)throw new TypeError("TAG_NAME is not set.");if(r===void 0)throw new TypeError("GITHUB_TOKEN is not set.");let n=`\u{1F680} Release: ${e}`,s=`This PR updates the \`package.json\` version and \`CHANGELOG.md\` for the new release **${e}**.`,a="main",c=`release-${e}`;i(r).rest.pulls.create({owner:t.repo.owner,repo:t.repo.repo,title:n,body:s,base:a,head:c}).then(o=>{console.log(`PR created: ${o.data.html_url||"N/A"}`)}).catch(o=>{console.error("Failed to create PR:",o),process.exit(1)})}p();
+// src/index.ts
+import { context, getOctokit } from "@actions/github";
+var { GITHUB_TOKEN, TAG_NAME } = process.env;
+async function run() {
+  if (TAG_NAME === void 0) throw new TypeError("TAG_NAME is not set.");
+  if (GITHUB_TOKEN === void 0) throw new TypeError("GITHUB_TOKEN is not set.");
+  const prTitle = `\u{1F680} Release: ${TAG_NAME}`;
+  const prBody = `This PR updates the \`package.json\` version and \`CHANGELOG.md\` for the new release **${TAG_NAME}**.`;
+  const baseBranch = "main";
+  const headBranch = `release-${TAG_NAME}`;
+  getOctokit(GITHUB_TOKEN).rest.pulls.create({
+    owner: context.repo.owner,
+    repo: context.repo.repo,
+    title: prTitle,
+    body: prBody,
+    base: baseBranch,
+    head: headBranch
+  }).then((response) => {
+    console.log(`PR created: ${response.data.html_url || "N/A"}`);
+  }).catch((error) => {
+    console.error("Failed to create PR:", error);
+    process.exit(1);
+  });
+}
+run();
