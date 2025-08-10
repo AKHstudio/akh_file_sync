ef4dca3bd84d4ef9ff96b8ba9aaf0910e3c35729
takokun <mukoudakota8888@gmail.com>
2025-08-10 16:52:24 +0900

    Fix watch.ts
    監視中に連続して保存するとそれ以上上手く動悸されていなかった不具合を修正

--- src/commands/watch.ts
+++ src/commands/watch.ts
@@ -5,6 +5,8 @@ import * as env from '@/index.js';
 
 class WatchCommand extends SyncCommand {
     private watchDirectories: string[];
+    private watchDelay: number = 2000; // Delay in milliseconds
+    private isWatchDelaying: boolean = false;
 
     constructor(directories: string[], options: { development: boolean; build: boolean; only: 'behavior' | 'resource' | undefined }) {
         super(directories, options);
@@ -15,8 +17,22 @@ class WatchCommand extends SyncCommand {
     public override async execute() {
         console.log('Watching...', this.watchDirectories);
         chokidar.watch(this.watchDirectories).on('change', async (path) => {
+            if (this.isWatchDelaying) {
+                console.log('🟡 Watch is delaying, skipping change:', path);
+                return;
+            }
+
             console.log('🟢 change', path);
+            this.isWatchDelaying = true; // Set the flag to true to prevent further changes from being processed immediately
             await super.execute();
+            console.log('🔄 Sync completed');
+
+            new Promise((resolve) => {
+                setTimeout(() => {
+                    this.isWatchDelaying = false;
+                    resolve(true);
+                }, this.watchDelay);
+            }); // Delay to prevent rapid changes
         });
 
         process.on('SIGINT', () => {
